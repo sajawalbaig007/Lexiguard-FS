7b3886d2366adee410837f1f02a00afe04321793
Sajawal Baig <sajawalbaig007@gmail.com>
2026-03-31 16:50:51 -0700

    change-1

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -11,14 +11,12 @@
 // export default router;
 
 
-// routes/authRoutes.js
 import express from "express";
-import { sendVerificationCode, verifyRegister, loginUser, googleAuth } from "../controllers/authController.js";
+import { registerUser, loginUser, googleAuth } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/send-code", sendVerificationCode);
-router.post("/verify-register", verifyRegister);
+router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/google", googleAuth);
 
