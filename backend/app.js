f58b64e78b03f56c5cc0f8d40224271994ab3c36
Sajawal Baig <sajawalbaig007@gmail.com>
2026-03-31 01:42:50 -0700

    first commit

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,13 @@ import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://lexiguard-fs.vercel.app',
+    'http://localhost:3000'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
