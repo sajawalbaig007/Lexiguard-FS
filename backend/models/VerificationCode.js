3799fbbf505e33ddfbff466832432600cd98e0b3
Sajawal Baig <sajawalbaig007@gmail.com>
2026-03-31 16:47:16 -0700

    changes-1

--- backend/models/VerificationCode.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const VerificationCodeSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  code: { type: String, required: true },
-  expiresAt: { type: Date, required: true },
-});
-
-export default mongoose.model("VerificationCode", VerificationCodeSchema);
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -11,12 +11,14 @@
 // export default router;
 
 
+// routes/authRoutes.js
 import express from "express";
-import { registerUser, loginUser, googleAuth } from "../controllers/authController.js";
+import { sendVerificationCode, verifyRegister, loginUser, googleAuth } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post("/send-code", sendVerificationCode);
+router.post("/verify-register", verifyRegister);
 router.post("/login", loginUser);
 router.post("/google", googleAuth);
 
